d301ef78702927a4bcf05870be5f55c4335b67df
Ihor Solodukha <igor.soloduha@gmail.com>
2024-04-01 17:14:01 +0200

    Update App.java

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class App {
 
     public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
+        SpringApplication app = new SpringApplication(App.class);
+        app.run(args);
     }
 }
